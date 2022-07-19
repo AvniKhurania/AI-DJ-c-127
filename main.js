0c085dbd810200a8de6376e6a952407348174694
AvniKhurania <89643274+AvniKhurania@users.noreply.github.com>
2022-07-19 19:31:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,28 @@ function modelLoaded()
 
 
 
+
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist="+scoreRightWrist+"scoreLeftWrist="+scoreLeftWrist);
+        
+        rightWristx=results[0].pose.rightWrist.x;
+        rightWristy=results[0].pose.rightWrist.y;
+        console.log("rightWristx="+rightWristx+"rightWristy="+rightWristy);
+
+        leftWristx=results[0].pose.leftWrist.x;
+        leftWristy=results[0].pose.leftWrist.y;
+        console.log("leftWristx="+leftWristx+"leftWristy="+leftWristy);
+
+
+    }
 }
 
 
@@ -47,4 +69,48 @@ function play()
 function draw()
 {
     image(video,0,0,600,500);
+    fill("#a42cd9");
+    stroke("#a42cd9");
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristx,rightWristy,20);
+
+        if(rightWristy>0 && rightWristy<100)
+        {
+            document.getElementById("speed").innerHTML="speed=0.5x";
+            song.rate(0.5);
+
+        }
+
+        else if(rightWristy>100 && rightWristy<200)
+        {
+            document.getElementById("speed").innerHTML="spped=1x";
+            song.rate(1);
+
+        }
+
+        else if(rightWristy>200 && rightWristy<300)
+        {
+            document.getElementById("speed").innerHTML="speed=1.5x";
+            song.rate(1.5);
+
+        }
+
+        else if(rightWristy>300 && rightWristy<400)
+        {
+            document.getElementById("speed").innerHTML="speed=2x";
+            song.rate(2);
+
+        }
+
+        else if(rightWristy>400)
+        {
+            document.getElementById("speed").innerHTML="speed=2.5x";
+            song.rate(2.5);
+            
+        }
+
+    }
+
+
 }
